d27865fbc6a4d211f3cf4d3d8d49f3cef155b439
denghang <denghang1989@163.com>
2019-04-23 22:32:18 +0800

    system

--- src/page/System.vue
+++ src/page/System.vue
@@ -1,13 +1,103 @@
 <template>
-  <div class="system">system</div>
+  <div class="system">
+    <ul class="system-list">
+      <li class="system-item" v-for="(item,index) in systemList" :key="index">
+        <section class="system-item-left">
+          <header>
+            {{item.name}}
+          </header>
+          <main>
+            <span class="system-item-subItem" v-for="(subItem,subIndex) in item.children" :key="subIndex">
+              {{subItem.name}}
+            </span>
+          </main>
+        </section>
+        <div class="system-item-right">
+          <i class="fa fa-angle-right"></i>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "System"
+    name: "System",
+    data() {
+      return {
+        systemObject: {}
+      }
+    },
+    computed: {
+      systemList() {
+        var systemList = [];
+        if (!(JSON.stringify(this.systemObject) === "{}")) {
+          systemList = this.systemObject.data;
+        }
+        return systemList;
+      }
+    },
+    methods: {
+      getSystemData() {
+        this.$http("/api/tree/json").then(({data}) => {
+          if (data.errorCode === 0) {
+            this.systemObject = data;
+          }
+        })
+      }
+    },
+    mounted() {
+      this.getSystemData()
+    }
   }
 </script>
 
 <style scoped>
+  .system-list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  
+  .system-item {
+    padding: 3px 10px;
+    border-bottom: 1px solid #ddd;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+  }
+  
+  .system-item:last-child {
+    border-bottom: none;
+  }
+  
+  .system-item-left {
+    flex: 0 0 90%;
+  }
+  
+  .system-item-right {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+  }
+  
+  .system-item-left > header {
+    padding: 3px 0px;
+    font-size: 16px;
+    color: #000;
+  }
+  
+  .system-item-left > main {
+    padding-top: 10px;
+    padding-bottom: 2px;
+    font-size: 12px;
+    color: #333;
+  }
+  
+  .system-item-subItem {
+  
+  }
 
 </style>
